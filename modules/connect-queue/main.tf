01c5967907a3b29b9b2b873607dc395209d63a9f
cloudsheger <cloudsheger@gmail.com>
2023-03-16 14:22:41 -0400

    updated  module

--- modules/connect-queue/main.tf
+++ modules/connect-queue/main.tf
@@ -1,7 +1,8 @@
 resource "aws_connect_queue" "connect_queue" {
+  instance_id            = var.instance_id
   name                   = var.name
   description            = var.queue_description
-  tags                   = var.tags
   hours_of_operation_id  = var.hours_of_operation_id
-  instance_id            = var.instance_id
+
+  tags                   = var.tags
 }
